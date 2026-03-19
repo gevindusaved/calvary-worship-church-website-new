d491897fbf643c62281d29ccf2eddc709c503081
gevindusaved <gevinduobeyesekera@example.com>
2026-03-19 11:38:47 +0530

    clean tailwind v4 setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-slate-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Tailwind if Working Correctly
+        </h1>
+      </div>
+    </>
+  )
+}
